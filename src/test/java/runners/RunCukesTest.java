97db1e0ee3e3664412a10584be89c98e163cef58
sigit.apri <sigit.apri@koltiva.com>
2026-08-31 22:54:27 +0700

    update tag on runners

--- src/test/java/runners/RunCukesTest.java
+++ src/test/java/runners/RunCukesTest.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         },
         features = {"src/test/resources/features"},
         plugin = {"json:target/cucumber.json"},
-        tags = "@signUp"
+        tags = "@regression"
 )
 public class RunCukesTest {
 
